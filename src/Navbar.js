bff771945badafebb6dd0fb7990f9ee690b31789
Eric Owusu <llkoole7@gmail.com>
2019-12-10 21:42:32 -0800

    Add Select and Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import Tooltip from 'rc-tooltip';
@@ -26,14 +28,25 @@ const handle = props => {
 const wrapperStyle = { width: 340, margin: 7 };
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: 'hex' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <a href="#">reactcolorpicker</a>
         </div>
-        <span className="level-value">Level: {level}</span>
+        <span>Level: {level}</span>
         <div className="slider-container" style={wrapperStyle}>
           <div className="slider">
             <Slider
@@ -46,20 +59,27 @@ class Navbar extends Component {
               trackStyle={{ backgroundColor: 'transparent', height: 5 }}
               handleStyle={{
                 backgroundColor: '#130e3a',
-                height: 18,
-                width: 18,
+                height: 17,
+                width: 17,
                 outline: 'none',
                 border: '2px solid #130e3',
                 boxShadow: 'none'
               }}
               railStyle={{
                 backgroundColor: '#d6d8db',
-                height: 10,
+                height: 7,
                 width: 340,
                 display: 'inline-block'
               }}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
