340df806c476a518dc715bbe0e8f6aea43a56ac4
Eric Owusu <llkoole7@gmail.com>
2019-12-15 15:07:40 -0800

    Updates

---
+++ precache-manifest.147c807ac90f9f85b158e6f4b8ea4a97.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a57076bd11764319e67f586ada13d2e",
+    "url": "/colors_App/index.html"
+  },
+  {
+    "revision": "7e283aadf1ac6fd0f7e7",
+    "url": "/colors_App/static/css/2.30d4ff4e.chunk.css"
+  },
+  {
+    "revision": "f4dbd8a3bc24ba14302d",
+    "url": "/colors_App/static/css/main.4dc90410.chunk.css"
+  },
+  {
+    "revision": "7e283aadf1ac6fd0f7e7",
+    "url": "/colors_App/static/js/2.60574300.chunk.js"
+  },
+  {
+    "revision": "a78b2cc5023c84a7d8b6a90f156ea821",
+    "url": "/colors_App/static/js/2.60574300.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4dbd8a3bc24ba14302d",
+    "url": "/colors_App/static/js/main.6786da9b.chunk.js"
+  },
+  {
+    "revision": "90aa9534c8ff183b82bb",
+    "url": "/colors_App/static/js/runtime-main.478ac93d.js"
+  },
+  {
+    "revision": "83fc06a136d657e5be42dbff8f057b5d",
+    "url": "/colors_App/static/media/bg.83fc06a1.svg"
+  },
+  {
+    "revision": "5b81fc60c68959a58cb002e33dedda6c",
+    "url": "/colors_App/static/media/bg2.5b81fc60.svg"
+  },
+  {
+    "revision": "c7607c2e137748ccfde251d0f6719aa6",
+    "url": "/colors_App/static/media/bg3.c7607c2e.svg"
+  },
+  {
+    "revision": "802beda68c20ba1497ce917d33286395",
+    "url": "/colors_App/static/media/bg4.802beda6.svg"
+  }
+]);
